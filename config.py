8923d88c6a792f5db7f9374139f4b42b70711661
sangjaekang <sangjaekang48@gmail.com>
2017-08-12 15:12:32 +0900

    config 파일 생성

---
+++ config.py
@@ -0,0 +1,21 @@
+#-*-encoding:utf-8-*-
+'''
+연산 처리에 이용되는 주요 constants들을 저장하는 파일
+'''
+
+# 1. computer spec 관련된 상수
+
+NUM_CORES = 4 # multi-processing 시 사용할 cpu core 수
+
+# 2. data spec 관련된 상수
+
+DELIM = '\t' # data의 delimiter / sep 에 해당하는 상수
+
+# 3. TEMP_FILE 관련된 상수
+
+
+
+# 4. DEBUG 관련된 상수
+
+DEBUG_PRINT = True
+
