3fffe7ecb4781ade71d4c5e0d2465f0830503b89
sangjaekang <sangjaekang48@gmail.com>
2017-08-12 14:48:28 +0900

    dataframe 크기 세는 거 skip 기능 추가

--- data/sampling_data.py
+++ data/sampling_data.py
@@ -6,12 +6,13 @@ INPUT_PATH  = None
 OUTPUT_PATH = None
 LINE_COUNTS = 100000
 DELIM       = ','
+SKIP        = False
 
 def _check_size(file_path):
     if file_path is None:
         raise ValueError('no file path')
 
-    f = open(file_path,mode='r',encoding="utf-8")
+    f = open(file_path,mode='r')
     col_num = f.readline().count(DELIM)+1
     row_num = sum(1 for _ in f)
 
@@ -19,14 +20,18 @@ def _check_size(file_path):
             format(row_num,col_num))
 
 def _set_parser():
-    global INPUT_PATH, OUTPUT_PATH, LINE_COUNTS
+    global SKIP
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-i', help="sampling data path")
     parser.add_argument('-o', help="output path")
     parser.add_argument('-l', help="line number")
+    parser.add_argument('-skip', help="skip count dataframe")
 
     args = parser.parse_args()
+
+    if args.skip:
+        SKIP = True
 
     return args
 
@@ -43,7 +48,8 @@ def run(input_path,output_path,line_counts):
     if line_counts:
         LINE_COUNTS = int(line_counts)
 
-    _check_size(INPUT_PATH) # 데이터 사이즈 확인
+    if not SKIP:
+        _check_size(INPUT_PATH) # 데이터 사이즈 확인
 
     if os.path.isfile(OUTPUT_PATH):
         os.remove(OUTPUT_PATH)
