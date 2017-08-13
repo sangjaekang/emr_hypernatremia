980f94985597e2ac971b8182eb989519d13ded46
sangjaekang <sangjaekang48@gmail.com>
2017-08-13 05:25:34 -0400

    config 파일　수정

--- mapapplier/config.py
+++ mapapplier/config.py
@@ -1,19 +1,18 @@
 # 1. related to computer spec 
 
 CHUNK_SIZE = 50000000
-DELIM = ','
-COL_NAME = ['no','KCD_code','context','context_check','date']
-USE_COLS = ['no','KCD_code','date']
 
 # 2. related to data spec 
 
 PATIENT_NUM = 5400000 # the number of patients
+COL_NAME = ['no','KCD_code','context','context_check','date']
+USE_COLS = ['no','KCD_code','date']
 DELIM = ',' # dataÀÇ delimiter / sep ¿¡ ÇØ´çÇÏ´Â »ó¼ö
 DROP_RATE = 10000 # the rate value to determine whether code is drop or not
 
 # 3. related to TEMP_FILE path
 
 KCD_OUTPUT_PATH = '../data/KCD_mapping_df.csv'
-DIAGNOSIS_OUTPUT_PATH = '../data/pre_prescribe_df.csv'
+DIAGNOSIS_OUTPUT_PATH = '../data/pre_diagnosis_df.csv'
 KCD_COUNTS_PATH = '../data/KCD_count_df.csv'
 TEMP_PATH = '../data/TEMP_df.csv'
