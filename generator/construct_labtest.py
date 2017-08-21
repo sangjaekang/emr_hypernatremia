c2379e86cec38706ce4fd8faa2ba425439adc8a0
sangjaekang <sangjaekang48@gmail.com>
2017-08-21 13:34:54 +0900

    labtest basic imputation

--- generator/construct_labtest.py
+++ generator/construct_labtest.py
@@ -19,6 +19,7 @@ def set_labtest_row():
     OFFSET_labtest_COUNTS 기준에　따라서，drop 할　row을　결정
     drop하고　남은　row를　metadata/usecol에　저장
     labtest는　labtest 별로　hdf5에　따로　저장되어　있음
+    그래서　get_index_map으로　부르지않음
     '''
     global labtest_output_path, OFFSET_LABTEST_COUNTS
     PREP_OUTPUT_DIR = check_directory(PREP_OUTPUT_DIR)
@@ -45,6 +46,9 @@ def set_labtest_row():
 
 
 def get_labtest_df(no): 
+    '''
+    환자번호를　넣으면　column은　KCDcode, row는　time-serial의　형태인　dataframe이　나오는　함수
+    '''
     global labtest_output_path
     store_lab = pd.HDFStore(labtest_output_path)
     
@@ -55,13 +59,33 @@ def get_labtest_df(no):
     # create empty dataframe
     use_labtest_values = store_lab.select('metadata/usecol').col.values
     result_df = pd.DataFrame(columns=col_list, index=use_labtest_values)
-    
+
     lab_node = store_lab.get_node('data')
     for lab_name in lab_node._v_children.keys():
+        result_lab_series = result_df.loc[lab_name]
         target_df = store_lab.select('data/{}'.format(lab_name),where='no=={}'.format(no))
+        target_df = target_df.groupby(['no','date']).mean().reset_index() # 같은달에 한번 이상 했을 시, 결과의 평균으로 저장
+        
+        pre_value = None
         for value in target_df.values:
             _, _date, _result = value
-            result_df.loc[lab_name].loc[_date] = _result
+            if pre_value is not None:
+                min_time = pre_value[1];max_time =_date
+                min_value = pre_value[2];max_value = _result
+                inter_time = get_time_interval(min_time,max_time)
+                for i in result_lab_series.loc[pre_value[1]:_date].index:
+                    intpol_time = get_time_interval(min_time,i) / inter_time
+                    intpol_value = (max_value-min_value)*intpol_time + min_value
+                    result_df.loc[lab_name].loc[i] = intpol_value
+                pre_value = value
+            else :
+                result_df.loc[lab_name].loc[:_date] = _result
+                pre_value = value
+
+        missing_index = result_lab_series[result_lab_series.isnull()].index
+        if missing_index.min() > col_list[0]:
+            last_loc = result_df.columns.get_loc(missing_index.min()) - 1
+            result_df.loc[lab_name].iloc[last_loc+1:]= result_df.loc[lab_name].iloc[last_loc]
 
     del target_df
 
