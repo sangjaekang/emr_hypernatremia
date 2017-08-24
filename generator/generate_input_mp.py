448d153403aaff2e1ae9930fd37c80b9df93b539
sangjaekang <sangjaekang48@gmail.com>
2017-08-24 16:19:41 +0900

    apply -> apply_async

--- generator/generate_input_mp.py
+++ generator/generate_input_mp.py
@@ -23,32 +23,32 @@ if __name__ == "__main__":
     a = sample_store.select('data/na_label')
     sample_store.close()
 
-    target_0_1 = a[a.label==0].no.unique()[:10000]
-    target_0_2 = a[a.label==0].no.unique()[10000:20000]
+    target_0_1 = a[a.label==0].no.unique()[:6000]
+    target_0_2 = a[a.label==0].no.unique()[6000:12000]
 
-    target_1_1 = a[a.label==1].no.unique()[:10000]
-    target_1_2 = a[a.label==1].no.unique()[10000:20000]
+    target_1_1 = a[a.label==1].no.unique()[:6000]
+    target_1_2 = a[a.label==1].no.unique()[6000:12000]
 
-    target_2_1 = a[a.label==2].no.unique()[:10000]
-    target_2_2 = a[a.label==2].no.unique()[10000:20000]
+    target_2_1 = a[a.label==2].no.unique()[:6000]
+    target_2_2 = a[a.label==2].no.unique()[6000:12000]
 
-    target_3_1 = a[a.label==3].no.unique()[:10000]
+    target_3_1 = a[a.label==3].no.unique()[:7000]
 
     print("Creating pool with 7 workers")
     pool = multiprocessing.Pool(processes=7)
     print(pool._pool)
     print("Invoking apply(save_patient_input, no_range)")
 
-    pool.apply(save_patient_input,[target_0_1])
-    pool.apply(save_patient_input,[target_0_2])
+    pool.apply_async(save_patient_input,[target_0_1])
+    pool.apply_async(save_patient_input,[target_0_2])
     
-    pool.apply(save_patient_input,[target_1_1])
-    pool.apply(save_patient_input,[target_1_2])
+    pool.apply_async(save_patient_input,[target_1_1])
+    pool.apply_async(save_patient_input,[target_1_2])
     
-    pool.apply(save_patient_input,[target_2_1])
-    pool.apply(save_patient_input,[target_2_2])
+    pool.apply_async(save_patient_input,[target_2_1])
+    pool.apply_async(save_patient_input,[target_2_2])
 
-    pool.apply(save_patient_input,[target_3_1])
+    pool.apply_async(save_patient_input,[target_3_1])
 
     pool.close()
     pool.join()
