32c6ff863bda5a5e80964b757ac92cbcdc6db88f
sangjaekang <sangjaekang48@gmail.com>
2017-08-29 20:49:16 +0900

    shell script conditional statement add

--- generator/generate_data_set.sh
+++ generator/generate_data_set.sh
@@ -1,31 +1,62 @@
 #!/bin/sh
 
 #　데이터　셋을　만드는　함수
+set_1=true
+set_2=false
+set_3=true
 
 ## OFFSET COUNTS 변화에　따른　데이터　셋
+
+if ["$set_1"=true] ; then
+
 echo "start offset COUNTS!-1" > ./process.txt
-python generate_input_mp.py ../data/input/dataset_2 na_label 4 7 4000 6 1 3 25 50
+rm -rf ../data/input/dataset_2
+python generate_input_mp.py ../data/input/dataset_2 na_label 4 7 4000 6 1 3 5 25
+
 echo "start offset COUNTS!-2" >> ./process.txt
-python generate_input_mp.py ../data/input/dataset_3 na_label 4 7 4000 6 1 3 50 75
+rm -rf ../data/input/dataset_3
+python generate_input_mp.py ../data/input/dataset_3 na_label 4 7 4000 6 1 3 25 50
+
 echo "start offset COUNTS!-3" >> ./process.txt
-python generate_input_mp.py ../data/input/dataset_4 na_label 4 7 4000 6 1 3 75 100
+rm -rf ../data/input/dataset_4
+python generate_input_mp.py ../data/input/dataset_4 na_label 4 7 4000 6 1 3 50 75
+
 echo "start offset COUNTS! end" >> ./process.txt
+fi
 
 ## gap period 변화에　따른　데이터　셋
+
+if ["$set_2"=true]; then
+
 echo "start gap period!-1" >> ./process.txt 
+rm -rf ../data/input/dataset_5
 python generate_input_mp.py ../data/input/dataset_5 na_label 4 7 4000 6 2 3 100 1000
+
 echo "start gap period!-2" >> ./process.txt 
+rm -rf ../data/input/dataset_6
 python generate_input_mp.py ../data/input/dataset_6 na_label 4 7 4000 6 3 3 100 1000
+
 echo "start gap period!-3" >> ./process.txt 
+rm -rf ../data/input/dataset_7
 python generate_input_mp.py ../data/input/dataset_7 na_label 4 7 4000 6 6 3 100 1000
+
 echo "gap period end!" >> ./process.txt 
+fi
 
 ## time length 변화에　따른　데이터셋
-echo "start time length!-1" >> ./process.txt
-python generate_input_mp.py ../data/input/dataset_8 na_label 4 7 4000 6 1 3 20 50
-echo "start time length!-2" >> ./process.txt
-python generate_input_mp.py ../data/input/dataset_9 na_label 4 7 4000 6 1 3 20 50
-echo "start time length!-3" >> ./process.txt
-python generate_input_mp.py ../data/input/dataset_10 na_label 4 7 4000 6 1 3 20 50
-echo "time length end!" >> ./process.txt
 
+if ["$set_3"=true] ; then
+echo "start time length!-1" >> ./process.txt
+rm -rf ../data/input/dataset_8
+python generate_input_mp.py ../data/input/dataset_8 na_label 4 7 4000 6 1 3 100 1000
+
+echo "start time length!-2" >> ./process.txt
+rm -rf ../data/input/dataset_9
+python generate_input_mp.py ../data/input/dataset_9 na_label 4 7 4000 9 1 3 150 1000
+
+echo "start time length!-3" >> ./process.txt
+rm -rf ../data/input/dataset_10
+python generate_input_mp.py ../data/input/dataset_10 na_label 4 7 4000 12 1 3 200 1000
+
+echo "time length end!" >> ./process.txt
+fi
